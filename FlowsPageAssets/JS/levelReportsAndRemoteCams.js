ee0e785988e91a6879ecd4389b8530d51f33340d
Scott Gilbert <scottvtgilbert@gmail.com>
2023-07-20 20:38:34 -0400

    Updating reference images for Middlebury to be notice that new images are being established

--- FlowsPageAssets/JS/levelReportsAndRemoteCams.js
+++ FlowsPageAssets/JS/levelReportsAndRemoteCams.js
@@ -4,15 +4,15 @@ function loaderHide() {
 };
 
 
-async function getCamImages(){
-    let middImages = await fetch("https://river-cams-photo-fetch.herokuapp.com/photos?riverName=middlebury&quantity=1000")
-    let middImagesJSON = await middImages.json();
-    middImagesJSON.reverse();
-    let bbImages = await fetch("https://river-cams-photo-fetch.herokuapp.com/photos?riverName=bigbranch&quantity=1000")
-    let bbImagesJSON = await bbImages.json();
-    bbImagesJSON.reverse();
-    let camsContent = 
-`<p>These images are gathered from cell-enabled trail cams set to take images every 2hrs between 7am and 5pm</p>  
+async function getCamImages() {
+	let middImages = await fetch("https://river-cams-photo-fetch.herokuapp.com/photos?riverName=middlebury&quantity=1000")
+	let middImagesJSON = await middImages.json();
+	middImagesJSON.reverse();
+	let bbImages = await fetch("https://river-cams-photo-fetch.herokuapp.com/photos?riverName=bigbranch&quantity=1000")
+	let bbImagesJSON = await bbImages.json();
+	bbImagesJSON.reverse();
+	let camsContent =
+		`<p>These images are gathered from cell-enabled trail cams set to take images every 2hrs between 7am and 5pm</p>  
 		<p>Issues with these images?  Contact us at <a href='mailto:gopaddling@creekvt.com?subject=River Cameras'> gopaddling@creekvt.com</a></p>
 		<br>
 		<h4>Middlebury River</h4>
@@ -20,14 +20,17 @@ async function getCamImages(){
 		<br/>
 		<details>
 		<summary>Click For Reference Images</summary>
-		<div class='camImages'>
+		<!--<div class='camImages'>
 			<div class='refImage'><a href='./FlowsPageAssets/Images/RiverCamsReferenceImages/MiddLow.jpg' target='_blank'><img src='./FlowsPageAssets/Images/RiverCamsReferenceImages/MiddLow.jpg'></a></div>
 			<div class='refImage'><a href='./FlowsPageAssets/Images/RiverCamsReferenceImages/MiddMedium.jpg' target='_blank'><img src='./FlowsPageAssets/Images/RiverCamsReferenceImages/MiddMedium.jpg'></a></div>
 			<div class='refImage'><a href='./FlowsPageAssets/Images/RiverCamsReferenceImages/MiddHigh.jpg' target='_blank'><img src='./FlowsPageAssets/Images/RiverCamsReferenceImages/MiddHigh.jpg'></a></div>
 		</div>
 		<p>These references are for levels to be expected in the Gorge Proper.  <br/>1)  The rock should have at least some water on top and the surrounding rocks to be submerged for a minimum, if the rock is not visible it will be high.<br/> 2)  This rock should have at least some water all the way over it for a minimum.  Once there is no pillow on the upstream side it is high.  <br/>3)  Look for the smaller rocks to be just barely sitting in water for a minimum.  If they are submerged it is high.</p>
 		<p class='disclaimer'>**The Middlebury River is a dangerous river that is extremely level dependant.  The rocks in these images shift regularly thus correlating live images to these as a level reference should be done with extreme caution.
-		For a first trip down it is highly recommended you have somebody knowledgeable with the river show you down**</p>
+		For a first trip down it is highly recommended you have somebody knowledgeable with the river show you down**</p>-->
+				<br/><br/>
+				<p>Overnight on July 14th into July 15th 2023 the town of Ripton experienced flash flooding on a scale comparable if not worse than that seen during Irene.  As a result the Middlebury River changed significantly, including the gauges rocks used to judge level at this remote camera.  New reference images are currently being established and will be uploaded soon.</p>
+
 		</details>
 		<br>
 		<h5>Most Recent Images</h5>
@@ -60,14 +63,14 @@ async function getCamImages(){
 			<div class='camImage'><a href=${bbImagesJSON[3]} target='_blank'><img width='100%' src=${bbImagesJSON[3]}></a></div>
 		</div>
 		<br>`;
-document.getElementById("riverCams").innerHTML = camsContent;
-loaderHide();
+	document.getElementById("riverCams").innerHTML = camsContent;
+	loaderHide();
 }
 
 getCamImages();
 
 
-async function getLevelReports () {
+async function getLevelReports() {
 	let levelReports = await fetch("./FlowsPageAssets/PHP/levelReports.php");
 	if (levelReports.status == 200) {
 		levelReports = levelReports.text();
@@ -79,6 +82,6 @@ async function getLevelReports () {
 }
 
 let recentReports = getLevelReports();
-recentReports.then( response => {
+recentReports.then(response => {
 	document.getElementById("levelReports").innerHTML = response;
 });
